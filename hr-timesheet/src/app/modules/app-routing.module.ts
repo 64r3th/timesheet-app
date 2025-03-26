76b1c3bb915fbbd5a3152f72aa0585eb10efa7ee
64r3th <159493290+64r3th@users.noreply.github.com>
2025-03-26 12:42:01 -0600

    Completed Unit 04

--- hr-timesheet/src/app/modules/app-routing.module.ts
+++ hr-timesheet/src/app/modules/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AnalyticsComponent   } from '../components/analytics/analytics.component';
+import { TimesheetComponent   } from '../components/timesheet/timesheet.component';
+import { DepartmentsComponent } from '../components/departments/departments.component';
+
+const routes: Routes = [
+  { path: '',   redirectTo: 'departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'timesheet', component: TimesheetComponent },
+  { path: 'analytics', component: AnalyticsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
