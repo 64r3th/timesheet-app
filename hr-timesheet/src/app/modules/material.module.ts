87d9bd3d7b95a7d5f811d4be378856e7166f9b68
64r3th <159493290+64r3th@users.noreply.github.com>
2025-03-26 13:22:18 -0600

    Completed Unit 05.5

--- hr-timesheet/src/app/modules/material.module.ts
+++ hr-timesheet/src/app/modules/material.module.ts
@@ -1,24 +1,28 @@
 import { NgModule     } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule    } from '@angular/material/card';
+import { MatListModule    } from '@angular/material/list';
 import { MatIconModule    } from '@angular/material/icon';
 import { MatButtonModule  } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
     CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatToolbarModule,
   ],
   exports: [
-    MatToolbarModule,
-    MatButtonModule,
+    MatCardModule,
     MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule { }
