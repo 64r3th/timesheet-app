558c0e8c18e37593d7b9576b8585ce4ac2d1eb12
64r3th <159493290+64r3th@users.noreply.github.com>
2025-03-27 08:20:28 -0600

    Completed Unit 07 pt1

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl    } from '@angular/forms';
 
 import { Department } from '../../interfaces/department';
-import {DepartmentsService} from '../../services/departments.service';
+import { DepartmentsService } from '../../services/departments.service';
 
 @Component({
   selector: 'app-timesheet',
@@ -13,6 +14,7 @@ import {DepartmentsService} from '../../services/departments.service';
 export class TimesheetComponent implements OnInit {
   departments: Department[];
   department: Department;
+  employeeNameFC: FormControl<string> = new FormControl('');
 
   constructor(
     private route: ActivatedRoute,
--- hr-timesheet/src/app/modules/material.module.ts
+++ hr-timesheet/src/app/modules/material.module.ts
@@ -1,11 +1,14 @@
-import { NgModule     } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule            } from '@angular/core';
+import { CommonModule        } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { MatCardModule    } from '@angular/material/card';
-import { MatListModule    } from '@angular/material/list';
-import { MatIconModule    } from '@angular/material/icon';
-import { MatButtonModule  } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule      } from '@angular/material/card';
+import { MatListModule      } from '@angular/material/list';
+import { MatIconModule      } from '@angular/material/icon';
+import { MatInputModule     } from '@angular/material/input';
+import { MatButtonModule    } from '@angular/material/button';
+import { MatToolbarModule   } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
@@ -14,15 +17,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatIconModule,
     MatListModule,
+    MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ],
   exports: [
     MatCardModule,
     MatIconModule,
     MatListModule,
+    MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
   ]
 })
 export class MaterialModule { }
