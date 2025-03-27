e310f8742914bff17e289a711d1b9d1ec95dd189
64r3th <159493290+64r3th@users.noreply.github.com>
2025-03-27 08:26:40 -0600

    Completed Unit 07 pt2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl    } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
+import { Employee   } from '../../interfaces/employee';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
 
@@ -15,6 +16,8 @@ export class TimesheetComponent implements OnInit {
   departments: Department[];
   department: Department;
   employeeNameFC: FormControl<string> = new FormControl('');
+  employees: Employee[] = [];
+  employeeId = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,5 +27,20 @@ export class TimesheetComponent implements OnInit {
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+    }
   }
 }
