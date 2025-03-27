796c1408de81f78cc270f543db290ff6e4ad4152
64r3th <159493290+64r3th@users.noreply.github.com>
2025-03-27 09:51:02 -0600

    Completed Unit 09 pt2

--- hr-timesheet/src/app/components/analytics-table/analytics-table.component.ts
+++ hr-timesheet/src/app/components/analytics-table/analytics-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import { Employee } from '../../interfaces/employee';
 
@@ -9,6 +9,9 @@ import { Employee } from '../../interfaces/employee';
   styleUrl: './analytics-table.component.scss'
 })
 export class AnalyticsTableComponent {
+  @Input()
+  departmentId: string | undefined;
+
   weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
   employees: Employee[] = [];
   employeeData: Employee[] = [
@@ -65,4 +68,15 @@ export class AnalyticsTableComponent {
       wednesday: 5
     }
   ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.employees = this.employeeData.filter(employee => employee.departmentId === this.departmentId);
+  }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+      + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
 }
